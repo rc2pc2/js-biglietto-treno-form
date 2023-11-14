84b1afc4dfea6ed5c2b8014789539ca63068ae69
rc2pc2 <petriccarcc@gmail.com>
2023-11-14 10:06:31 +0100

    refactored without comments and unused locs

--- js/script.js
+++ js/script.js
@@ -3,38 +3,22 @@ const teenDiscount = 1.0 - 0.1765;
 const elderDiscount = 1.0 - 0.5327;
 
 const distanceInputEl = document.querySelector('input#input-distance');
-// const ageInputEl = document.querySelector('input#input-age');
 const ageGroupSelectEl = document.querySelector('select#select-age');
-const generatorButton = document.querySelector('button')
+const ticketGeneratorBtn = document.querySelector('button')
 
-// console.log(distanceInputEl, ageInputEl, generatorButton);
-
-//  ? ogni volta che clicco il bottone:
-generatorButton.addEventListener( 'click', function(){
-    // console.warn('click!');
-
-    // # prendo il value di input-distance
+ticketGeneratorBtn.addEventListener( 'click', function(){
     const distance = parseFloat(distanceInputEl.value);
-
-    // # prendo il value di input-age
     const ageGroup = parseInt(ageGroupSelectEl.value);
 
-    console.warn(distance, ageGroup);
-
-    // # calcolo il prezzo del biglietto:
     let totalPrice = pricePerKm * distance;
 
-    // # prezzoBiglietto * ( 1.0 - x) {in base all'eta'}
     if ( ageGroup === 1 ){
-        // totalPrice *= teenDiscount;
         totalPrice = totalPrice * teenDiscount;
     } else if ( ageGroup === 2 ){
-        // totalPrice *= elderDiscount;
         totalPrice = totalPrice * elderDiscount;
     }
-        // 17.65 minorenni, 53.27 anziani, costo base 0.1976 al km
-    // # scrivo in console il risultato
-    console.warn(totalPrice);
+
+    console.warn(distance, ageGroup, 'price: ',totalPrice);
 });
 
 
