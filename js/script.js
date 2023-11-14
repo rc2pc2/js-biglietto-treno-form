ff2f79fd301695a47888ed3f9e8dcc2a9f3fb18f
rc2pc2 <petriccarcc@gmail.com>
2023-11-14 10:02:11 +0100

    alternative solution using select instead of input

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ const teenDiscount = 1.0 - 0.1765;
 const elderDiscount = 1.0 - 0.5327;
 
 const distanceInputEl = document.querySelector('input#input-distance');
-const ageInputEl = document.querySelector('input#input-age');
+// const ageInputEl = document.querySelector('input#input-age');
+const ageGroupSelectEl = document.querySelector('select#select-age');
 const generatorButton = document.querySelector('button')
 
 // console.log(distanceInputEl, ageInputEl, generatorButton);
@@ -16,21 +17,21 @@ generatorButton.addEventListener( 'click', function(){
     const distance = parseFloat(distanceInputEl.value);
 
     // # prendo il value di input-age
-    const age = parseInt(ageInputEl.value);
+    const ageGroup = parseInt(ageGroupSelectEl.value);
 
-    console.warn(distance, age);
+    console.warn(distance, ageGroup);
 
     // # calcolo il prezzo del biglietto:
-        let totalPrice = pricePerKm * distance;
+    let totalPrice = pricePerKm * distance;
 
-        // # prezzoBiglietto * ( 1.0 - x) {in base all'eta'}
-        if ( age < 18 ){
-            // totalPrice *= teenDiscount;
-            totalPrice = totalPrice * teenDiscount;
-        } else if (age > 64 ){
-            // totalPrice *= elderDiscount;
-            totalPrice = totalPrice * elderDiscount;
-        }
+    // # prezzoBiglietto * ( 1.0 - x) {in base all'eta'}
+    if ( ageGroup === 1 ){
+        // totalPrice *= teenDiscount;
+        totalPrice = totalPrice * teenDiscount;
+    } else if ( ageGroup === 2 ){
+        // totalPrice *= elderDiscount;
+        totalPrice = totalPrice * elderDiscount;
+    }
         // 17.65 minorenni, 53.27 anziani, costo base 0.1976 al km
     // # scrivo in console il risultato
     console.warn(totalPrice);
