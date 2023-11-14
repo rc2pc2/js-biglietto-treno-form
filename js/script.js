1911062d87929573ab6d5215d6f19b404162e1b3
rc2pc2 <petriccarcc@gmail.com>
2023-11-14 10:28:23 +0100

    beautified html structure and exercise

--- js/script.js
+++ js/script.js
@@ -4,7 +4,8 @@ const elderDiscount = 1.0 - 0.5327;
 
 const distanceInputEl = document.querySelector('input#input-distance');
 const ageGroupSelectEl = document.querySelector('select#select-age');
-const ticketGeneratorBtn = document.querySelector('button')
+const ticketGeneratorBtn = document.querySelector('button');
+const outputElement = document.querySelector('#output');
 
 ticketGeneratorBtn.addEventListener( 'click', function(){
     const distance = parseFloat(distanceInputEl.value);
@@ -14,11 +15,15 @@ ticketGeneratorBtn.addEventListener( 'click', function(){
 
     if ( ageGroup === 1 ){
         totalPrice = totalPrice * teenDiscount;
+        outputElement.className = 'text-green';
     } else if ( ageGroup === 2 ){
         totalPrice = totalPrice * elderDiscount;
+        outputElement.className = 'text-yellow';
+    } else {
+        outputElement.className = 'text-seagreen';
     }
 
+
     console.warn(distance, ageGroup, 'price: ',totalPrice);
+    outputElement.innerHTML = totalPrice.toFixed(2) + '&euro;';
 });
-
-
